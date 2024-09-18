3d576e1361faf659fe5f96d11e7da45a98d44aba
farees-s <fareesisid@gmail.com>
2024-09-18 00:29:35 -0400

    added navi center button for babydith

--- PicklePlaySB V1/FindView.swift
+++ PicklePlaySB V1/FindView.swift
@@ -30,6 +30,9 @@ struct FindView: View {
                    }
                }
            }
+           .mapControls{
+               MapUserLocationButton()
+           }
            .mapStyle(.standard(elevation: .realistic))
            .onAppear {
                searchForPickleballCourts()
