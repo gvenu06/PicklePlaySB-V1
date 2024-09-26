b2fd69464218bb7c7e1e88a1197efe83a3278f50
farees-s <fareesisid@gmail.com>
2024-09-26 19:54:41 -0400

    more work on authentication my computer is about to die (3/? (estimated 4))

---
+++ PicklePlaySB V1/Authentication/SignInUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInUIView.swift
+//  PicklePlaySB V1
+//
+//  Created by Foyez Siddiqui on 9/26/24.
+//
+
+import SwiftUI
+
+struct SignInUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInUIView()
+}
