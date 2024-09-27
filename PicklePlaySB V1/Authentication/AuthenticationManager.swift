a22f96f9a9002ca85ac0f16a7f6c564727de0d77
farees-s <fareesisid@gmail.com>
2024-09-26 23:36:02 -0400

    authentication with email works (need to connect the jawns together but thats it), with google gmail integration next. adds email/password to firebase successfully. changed app logo. authentication (4/? (estimated 6))

---
+++ PicklePlaySB V1/Authentication/AuthenticationManager.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationManager.swift
+//  PicklePlaySB V1
+//
+//  Created by Foyez Siddiqui on 9/26/24.
+//
+
+import SwiftUI
+
+struct AuthenticationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationManager()
+}
