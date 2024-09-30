b072dc44b7ef8e9c0c770f6f04f8d5f5f1c5b05d
farees-s <fareesisid@gmail.com>
2024-09-30 01:31:44 -0400

    authentication should be fully functioning but one small error regarding signing out, basically one line code fix/mild restructuring, sign in and flow completely fine (after fixing). ready for db Authentication: basically done

---
+++ PicklePlaySB V1/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  PicklePlaySB V1
+//
+//  Created by Foyez Siddiqui on 9/30/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
