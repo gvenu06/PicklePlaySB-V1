b8bffaf68296f766aab1682854c87a245a8b83cd
farees-s <fareesisid@gmail.com>
2024-10-24 21:21:28 -0400

    ode ode ode ode ode errors + out of the resource which supplies help but a lot of progress working on queue system booking works everything works

---
+++ PicklePlaySB V1/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  PicklePlaySB V1
+//
+//  Created by Foyez Siddiqui on 10/24/24.
+//
+
+import SwiftUI
+
+struct AppDelegate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppDelegate()
+}
---
+++ PicklePlaySB V1/UsernameSetupView.swift
@@ -0,0 +1,18 @@
+//
+//  UsernameSetupView.swift
+//  PicklePlaySB V1
+//
+//  Created by Foyez Siddiqui on 10/24/24.
+//
+
+import SwiftUI
+
+struct UsernameSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsernameSetupView()
+}
